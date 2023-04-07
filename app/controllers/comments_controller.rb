51944518a937f05444ea3a4bb35bb8e0dd985d5a
JOSE MERCONCHINI <buddylee939@gmail.com>
2023-04-07 14:32:08 -0400

    Update comments form and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
+  before_action :set_post, only: [:create,:destroy]
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:name, :body))
+    @comment = @post.comments.create(comment_params)
 
     redirect_to post_path(@post)
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
     @comment.destroy
 
     redirect_to post_path(@post)
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:post_id, :body, :name)
+    end  
 end
+
